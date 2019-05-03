3a9d2fd1836795d79ffe0e91d5e606d610e7fb96
khoaLib <phamkhoa996@gmail.com>
2019-05-03 14:43:24 -0700

    aa

--- src/GameModel.java
+++ src/GameModel.java
@@ -4,83 +4,89 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class GameModel 
-{
+public class GameModel {
 	private ArrayList<StoneCluster> stoneClusters;
-	//private ArrayList<ShapePanel> holes;
+	// private ArrayList<ShapePanel> holes;
 	private ArrayList<ChangeListener> listeners;
 	private boolean playerATurn;
-	
+
 	/**
 	 * Constructs a GameModel
 	 */
-	public GameModel()
-	{
+	public GameModel() {
+		// true is A turn and false is B turn
+		playerATurn = true;
 		stoneClusters = new ArrayList<StoneCluster>(14);
-		for(int i = 0; i < 14; i++)
-		{
-			stoneClusters.add(new StoneCluster(0,0)); //add dummy clusters to start
+		for (int i = 0; i < 14; i++) {
+			stoneClusters.add(new StoneCluster(0, 0)); // add dummy clusters to start
 		}
-		//holes = new ArrayList<ShapePanel>();
+		// holes = new ArrayList<ShapePanel>();
 		listeners = new ArrayList<ChangeListener>();
 	}
-	
+
+	public void switchTurn() {
+		if (playerATurn == true) {
+			playerATurn = false;
+		} else {
+			playerATurn = true;
+		}
+	}
+
 	/**
 	 * Attach a listener to the model
-	 * @param cListener - the ChangeListener to set
+	 * 
+	 * @param cListener
+	 *            - the ChangeListener to set
 	 */
-	public void attach(ChangeListener cListener)
-	{
+	public void attach(ChangeListener cListener) {
 		listeners.add(cListener);
 	}
-	
-	public void addStoneCluster(StoneCluster sc, int index)
-	{
+
+	public void addStoneCluster(StoneCluster sc, int index) {
 		this.stoneClusters.set(index, sc);
 	}
 
-	public void pickUpStones(StoneCluster sc)
-	{
-		//DO LOGIC OF MANCALA HERE?
+	public void pickUpStones(StoneCluster sc) {
+		// DO LOGIC OF MANCALA HERE?
 		int stonesPickedUp = sc.getNumberOfStones();
-		sc.zeroStones();
 		
-		//Starts the loop one pit after the one clicked and moves however many stones were picked up
-		/*adds one stone to each pit visited.
-		for(int i = sc.getIndexInArray() + 1; i <= stonesPickedUp + sc.getIndexInArray(); i++)
-		{
-			stoneClusters.get(i%stoneClusters.size()).addOneStone();
-			
-		}
-		*/
-		
-		int currentIndex = sc.getIndexInArray() + 1; //starting point
-		while(currentIndex < sc.getIndexInArray()+1 + stonesPickedUp)
-		{
-			
-			stoneClusters.get(currentIndex%stoneClusters.size()).addOneStone();
-			
-			currentIndex++; //increment the index
-		}
-		
-		
-		for(ChangeListener l : listeners)
-		{
-			l.stateChanged(new ChangeEvent(this));
-		}
-		
-	}
-	
-	
-	
-	
-	
-	
-//--------------------GETTERS AND SETTERS-------------------------------	
-	
-	
-	
+		//System.out.println("--Player " + currentTurn + ".");
+		if ((sc.getIndexInArray() < 6 && playerATurn == true)
+				|| ((sc.getIndexInArray() > 6 && sc.getIndexInArray() < 13) && playerATurn == false)) {
+			// Starts the loop one pit after the one clicked and moves however many stones
+			// were picked up
+			/*
+			 * adds one stone to each pit visited. for(int i = sc.getIndexInArray() + 1; i
+			 * <= stonesPickedUp + sc.getIndexInArray(); i++) {
+			 * stoneClusters.get(i%stoneClusters.size()).addOneStone();
+			 * 
+			 * }
+			 */
+			int controlNum = sc.getIndexInArray() + stonesPickedUp;
+			if((controlNum % stoneClusters.size())!=6 &&(controlNum % stoneClusters.size())!=13) {
+				switchTurn();
+				System.out.println("Turn has been changed");
+			}
+				
+			sc.zeroStones();
+			int currentIndex = sc.getIndexInArray() + 1; // starting point
+			while (currentIndex < sc.getIndexInArray() + 1 + stonesPickedUp) {
 
+				stoneClusters.get(currentIndex % stoneClusters.size()).addOneStone();
+
+				currentIndex++; // increment the index
+			}
+
+			for (ChangeListener l : listeners) {
+				l.stateChanged(new ChangeEvent(this));
+			}
+		}else {
+			System.out.println("It's not your turn");
+		}
+
+	}
+
+	// --------------------GETTERS AND SETTERS-------------------------------
 
 	/**
 	 * @return the stoneClusters
@@ -90,24 +96,21 @@ public class GameModel
 	}
 
 	/**
-	 * @param stoneClusters the stoneClusters to set
+	 * @param stoneClusters
+	 *            the stoneClusters to set
 	 */
 	public void setStoneClusters(ArrayList<StoneCluster> stoneClusters) {
 		this.stoneClusters = stoneClusters;
 	}
-	
+
 	/**
-	 * @return the holes
-	public ArrayList<ShapePanel> getHoles() {
-		return holes;
-	}
-	/**
-	 * @param holes - the holes to set
+	 * @return the holes public ArrayList<ShapePanel> getHoles() { return holes; }
+	 *         /**
+	 * @param holes
+	 *            - the holes to set
 	 *
-	public void setHoles(ArrayList<ShapePanel> holes) {
-		this.holes = holes;
-	}
-	/**
+	 *            public void setHoles(ArrayList<ShapePanel> holes) { this.holes =
+	 *            holes; } /**
 	 * @return the listeners
 	 */
 	public ArrayList<ChangeListener> getListeners() {
@@ -115,12 +118,11 @@ public class GameModel
 	}
 
 	/**
-	 * @param listeners - the listeners to set
+	 * @param listeners
+	 *            - the listeners to set
 	 */
 	public void setListeners(ArrayList<ChangeListener> listeners) {
 		this.listeners = listeners;
 	}
 
-	
-	
 }
