5d16536879cef9c646b6f3acd9e39c35d0d87aa5
Alexis Huerta <alehuer90@gmail.com>
2019-05-04 20:00:52 -0700

    commented GUI classes

--- src/Pit.java
+++ src/Pit.java
@@ -5,23 +5,27 @@ import javax.swing.JPanel;
 
 /**
  * Defines the shape of the Pit
- * Can include more in here, like the number of stones in the pit..
+ * @author huerta, hsiao, pham
  *
  */
 public class Pit extends GameShape{
 
-	
-
+	/**
+	 * Constructor for Pit - adds the pit to the path
+	 * 
+	 */
 	public Pit()
 	{
-//		this.pitID = pitID;
-//		this.numberOfStones = numberOfStones;
-		
+
 		Ellipse2D.Double pit = new Ellipse2D.Double(5, 20, 100, 100*1.5);
 		
 		add(pit);
 	}
 	
+	/**
+	 * A method that can be called to return this Pit
+	 * @return
+	 */
 	public Pit getPitShape()
 	{
 		return this;
